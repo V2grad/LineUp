d64f38cc6f3e7cf6e980f59d74316c921bf0971b
Haoyu He <heh3@rpi.edu>
2019-04-22 22:08:46 -0400

    finish leave event card and form

--- src/modules/event/views/LeaveEvent.vue
+++ src/modules/event/views/LeaveEvent.vue
@@ -1,3 +1,24 @@
 <template>
-
+<div>
+    <ShowEventCard v-if="onGoingEvent"/>
+    <LeaveEventCard v-else/>
+</div>
 </template>
+
+<script>
+import LeaveEventCard from '../components/Cards/LeaveEventCard'
+import ShowEventCard from '../components/Cards/ShowEventCard'
+
+export default {
+  name: 'LeaveEvent',
+  components: {
+    LeaveEventCard,
+    ShowEventCard
+  },
+  computed: {
+    onGoingEvent () {
+      return this.$store.getters['event/onGoingEvent']
+    }
+  }
+}
+</script>
